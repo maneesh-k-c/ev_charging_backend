ba5437552e27f1c3dc22ca63879c305fec8c9c6d
maneeshk-chandran <maneeshchandran28@gmail.com>
2023-03-26 10:23:32 +0530

    login changes

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -7,159 +7,73 @@ const cstation = require('../models/chargingStationData')
 const sstation = require('../models/serviceStationData')
 const jwt = require('jsonwebtoken')
 
-LoginRouter.post('/',(req, res)=>{
-    console.log("logindata====",req.body)
-    let fetchedUser
-    login.findOne({username: req.body.username})
-    .then((user)=>{
-       
-        if(!user){
-            return res.status(401).json({
-                success:false,
-                error:true,
-                message:"User Not Found!"
-            })
-        }
-            fetchedUser = user
-            return bcrypt.compare(req.body.password, user.password)      
-    })
-    .then(result=>{
-        if(!result){
-            return res.status(401).json({
-                success:false,
-                error:true,
-                message:"Please Check Password!"
-            })
-        }
-        id = fetchedUser._id
-        console.log(fetchedUser);
-        if(fetchedUser.role=="0"){
-            login.findOne({login_id:id})
-                .then((registerData)=>{
-                    const token = jwt.sign(
-                        {username:fetchedUser.username, role:fetchedUser.role,
-                            userId:fetchedUser._id},
-                        "secret_this_should_be_longer",
-                        { expiresIn: "5h" }
-                    )
-                    res.status(200).json({
-                        success:true,
-                        error:false,
-                        token: token,
-                        expiresIn: 3600,
-                         role:fetchedUser.role,
-                        loginId: fetchedUser._id,
-                    })
-                })
-    }
-        if(fetchedUser.role=="1"){
-              if(fetchedUser.status=="1"){
-            console.log("approved");
-            cstation.findOne({login_id:id})
-            .then((registerData)=>{
-                const token = jwt.sign(
-                    {username:fetchedUser.username, role:fetchedUser.role,
-                        userId:fetchedUser._id},
-                    "secret_this_should_be_longer",
-                    { expiresIn: "5h" }
-                )
-                res.status(200).json({
-                    success:true,
-                    error:false,
-                    token: token,
-                    expiresIn: 3600,
-                    role:fetchedUser.role,
-                    chargingStationName:registerData.name,
-                    chargingStation_id:registerData._id,
-                    loginId: fetchedUser._id,
-                })
-            })
-     
-        } 
-        else{
-            res.status(200).json({
-                success:false,
-                error:true,
-              message:"request pending!!!!!!!!!!"
-            })
-        }
-        }
-        else if(fetchedUser.role=="2"){
-            if(fetchedUser.status=="1"){
-                console.log("approved");
-                userRegister.findOne({login_id:id})
-                .then((registerData)=>{
-                    // console.log("membersss",registerData.members.length);
-                    const token = jwt.sign(
-                        {username:fetchedUser.username, role:fetchedUser.role,
-                            userId:fetchedUser._id},
-                        "secret_this_should_be_longer",
-                        { expiresIn: "1h" }
-                    )
 
-                    res.status(200).json({
-                        success:true,
-                        error:false,
-                        token: token,
-                        expiresIn: 3600,
-                         role:fetchedUser.role,
-                        loginId: fetchedUser._id,
-                        name: registerData.name,
-                       
-                    })
-                })
-         
-            }
-            else{
-                res.status(200).json({
-                    success:false,
-                    error:true,
-                  message:"request pending!!!!!!!!!!"
-                })
-            }
-        }
-        else if(fetchedUser.role=="3"){
-            if(fetchedUser.status=="1"){
-                console.log("approved");
-                sstation.findOne({login_id:id})
-                .then((registerData)=>{
-                    const token = jwt.sign(
-                        {username:fetchedUser.username, userRole:fetchedUser.role,
-                            userId:fetchedUser._id},
-                        "secret_this_should_be_longer",
-                        { expiresIn: "5h" }
-                    )
-                    res.status(200).json({
-                        success:true,
-                        error:false,
-                        token: token,
-                        expiresIn: 3600,
-                         role:fetchedUser.role,
-                        loginId: fetchedUser._id,
-                        name:registerData.name,
-                        service_station_id:registerData._id,
-                      
-                    })
-                })
-         
-            }
-            else{
-                res.status(200).json({
-                    success:false,
-                    error:true,
-                  message:"request pending!!!!!!!!!!"
+LoginRouter.post("/", async (req, res) => {
+    const { username, password } = req.body;
+    console.log(username);
+
+    try {
+        const oldUser = await login.findOne({ username })
+        if (!oldUser) return res.status(404).json({ success: false, error: true, message: "User doesn't Exist" })
+        const isPasswordCorrect = await bcrypt.compare(password, oldUser.password)
+        console.log("user", isPasswordCorrect);
+
+        if (!isPasswordCorrect) return res.status(400).json({ success: false, error: true, message: "Incorrect password" })
+
+        if (oldUser.role === '1') {
+            const companyDetails = await Company.findOne({ login_id: oldUser._id })
+            if (companyDetails) {
+                return res.status(200).json({
+                    success: true,
+                    error: false,
+                    username: oldUser.username,
+                    role: oldUser.role,
+                    status: oldUser.status,
+                    login_id: oldUser._id,
+                    company_id: companyDetails._id
                 })
             }
         }
-       
-       
-       
-    })
-    .catch(err=>{
-        return res.status(401).json({
-            message: "Auth failed"
-        })
-    })
+        else if (oldUser.role === '2') {
+            if (oldUser.status == "1") {
+                const userDetails = userRegister.findOne({ login_id: oldUser._id })
+                if (userDetails) {
+                    res.status(200).json({
+                        success: true,
+                        error: false,
+                        username: oldUser.username,
+                        role: oldUser.role,
+                        status: oldUser.status,
+                        login_id: oldUser._id,
+                        name: userDetails.name,
+
+                    })
+                }
+            }
+            else {
+                res.status(200).json({
+                    success: false,
+                    error: true,
+                    login_id: oldUser._id,
+                    message: "waiting for admins approval"
+                })
+            }
+        }
+        else {
+
+            return res.status(200).json({
+                success: true,
+                error: false,
+                username: oldUser.username,
+                login_id: oldUser._id,
+                status: oldUser.status,
+                role: oldUser.role,
+            })
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
 })
+
 
 module.exports = LoginRouter
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -172,88 +172,34 @@ userRouter.post('/service-station-register', (req, res) => {
 
 })
 
-userRouter.post('/charging-station-register', (req, res) => {
+userRouter.post('/charging-station-register', async(req, res) => {
     console.log("data " + JSON.stringify(req.body))
-    bcrypt.hash(req.body.password, 10, function (err, hashedPass) {
-        if (err) {
-            return res.status(400).json({
-                success: false,
-                error: true,
-                message: 'password hashing error'
-            })
+    try {
+        const oldUser = await login.findOne({ username: req.body.username });
+        if (oldUser) {
+            return res.status(400).json({ success: false, error: true, message: "User already exists" });
         }
-        let logindata = {
-            username: req.body.email,
-            password: hashedPass,
-            role: 1,
-            status: 0
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        const oldphone = await register.findOne({ phone_no: req.body.phone_no });
+        if (oldphone) {
+            return res.status(400).json({ success: false, error: true, message: "Phone number already exists" });
         }
-        login.findOne({ username: req.body.username })
-            .then(username => {
-                if (username) {
-                    return res.status(400).json({
-                        success: false,
-                        error: true,
-                        message: 'email already exist!'
-                    })
-                }
-                else {
-                    var item = login(logindata)
-                    item.save()
-                        .then(() => {
-                            login.findOne({ username: logindata.username })
-                                .then(function (details) {
-                                    var id = details._id
-                                    let registerdata = {
-                                        login_id: id,
-                                        name: req.body.name,
-                                        email: req.body.email,
-                                        location: req.body.location.toLowerCase(),
-                                        contact_no: req.body.contact_no,
-                                        slots: req.body.slots,
+        const oldemail = await register.findOne({ email: req.body.email });
+        if (oldemail) {
+            return res.status(400).json({ success: false, error: true, message: "Email id already exists" });
+        }
+        var log = { username: req.body.username, password: hashedPassword, role: 1, status: 1 }
+        const result = await login(log).save()
+        var reg ={login_id: result._id, name: req.body.name,email: req.body.email,phone_no: req.body.phone_no,location: req.body.location,address: req.body.address,} 
+        const result2 = await register(reg).save()
+        if (result2) {
+            res.status(201).json({ success: true, error: false, message: "Registration completed", details: result2 });
+        }
 
-                                    }
-                                    charging.findOne({ contact_no: registerdata.contact_no })
-                                        .then((mobile) => {
-                                            if (!mobile) {
-
-                                                var register_item = charging(registerdata)
-                                                register_item.save()
-                                                    .then(() => {
-                                                        res.status(200).json({
-                                                            success: true,
-                                                            error: false,
-                                                            message: 'registration success'
-                                                        })
-                                                    })
-
-                                            }
-                                            else {
-                                                console.log(id)
-                                                login.deleteOne({ _id: id })
-                                                    .then(() => {
-
-                                                        res.status(401).json({
-                                                            success: false,
-                                                            error: true,
-                                                            message: 'Mobile number is already registered with us'
-                                                        })
-
-
-                                                    })
-
-                                            }
-                                        })
-
-
-                                })
-
-                        })
-
-                }
-
-            })
-    })
+    } catch (error) {
+        res.status(500).json({ success: false, error: true, message: "Something went wrong" });
+        console.log(error);
+    }
 
 })
 
@@ -265,7 +211,7 @@ userRouter.post('/register', async (req, res) => {
             return res.status(400).json({ success: false, error: true, message: "User already exists" });
         }
         const hashedPassword = await bcrypt.hash(req.body.password, 12);
-        const oldphone = await register.findOne({ phone: req.body.phone });
+        const oldphone = await register.findOne({ phone_no: req.body.phone_no });
         if (oldphone) {
             return res.status(400).json({ success: false, error: true, message: "Phone number already exists" });
         }
@@ -273,7 +219,7 @@ userRouter.post('/register', async (req, res) => {
         if (oldemail) {
             return res.status(400).json({ success: false, error: true, message: "Email id already exists" });
         }
-        var log = { username: req.body.username, password: hashedPassword, role: 2, status: 1 }
+        var log = { username: req.body.username, password: hashedPassword, role: 2, status: 0 }
         const result = await login(log).save()
         var reg ={login_id: result._id, name: req.body.name,email: req.body.email,phone_no: req.body.phone_no,location: req.body.location,address: req.body.address,} 
         const result2 = await register(reg).save()
